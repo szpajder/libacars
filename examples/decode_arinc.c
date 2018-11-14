aba04e45f8795a958ae317cba7674448f0cf6e3b
Tomasz Lemiech <szpajder@gmail.com>
2018-11-14 21:27:57 +0100

    examples: added decode_arinc and some cleanups

--- examples/decode_arinc.c
+++ examples/decode_arinc.c
@@ -1,5 +1,5 @@
 /*
- *  decpdlc - a simple FANS-1/A CPDLC message decoder
+ *  decode_arinc - an example of ARINC 622 ATS message decoder
  *
  *  Copyright (c) 2018 Tomasz Lemiech <szpajder@gmail.com>
  *
@@ -16,85 +16,43 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
+#include <stdlib.h>		// getenv()
 #include <string.h>
-#include <stdbool.h>
 #include <libacars.h>
-#include <cpdlc.h>
-#include <util.h>		// la_slurp_hexstring()
-#include <vstring.h>		// la_vstring
+#include <arinc.h>
+#include <vstring.h>
 
 void usage() {
 	fprintf(stderr,
-	"decpdlc - an example program for decoding FANS-1/A CPDLC messages in ACARS text\n\n"
-	"(c) 2018 Tomasz Lemiech <szpajder@gmail.com>\n"
+	"decode_arinc - an example of ARINC 622 ATS message decoder (CPDLC, ADS-C)\n"
+	"(c) 2018 Tomasz Lemiech <szpajder@gmail.com>\n\n"
 	"Usage:\n\n"
 	"To decode a single message from command line:\n\n"
-	"\t./decpdlc <direction> <acars_message_text>\n\n"
+	"\t./decode_arinc <direction> <acars_message_text>\n\n"
 	"where <direction> is one of:\n"
 	"\tu - means \"uplink\" (ground-to-air message)\n"
 	"\td - means \"downlink\" (air-to-ground message)\n\n"
 	"Enclose ACARS message text in quotes if it contains spaces or other shell\n"
 	"special shell characters, like '#'.\n\n"
-	"Example: ./decpdlc u '- #MD/AA ATLTWXA.CR1.N7881A203A44E8E5C1A932E80E'\n\n"
+	"Example: ./decode_arinc d '/BOMASAI.ADS.VT-ANB072501A070A988CA73248F0E5DC10200000F5EE1ABC000102B885E0A19F5'\n\n"
 	"To decode multiple messages from a text file:\n\n"
 	"1. Prepare a file with multiple messages, one per line. Precede each line\n"
 	"   with 'u' or 'd' (to indicate message direction) and a space. Direction\n"
 	"   indicator must appear as a first character on the line (no preceding\n"
 	"   spaces please). Example:\n\n"
 	"u /AKLCDYA.AT1.9M-MTB215B659D84995674293583561CB9906744E9AF40F9EB\n"
-	"u /AKLCDYA.AT1.B-27372142ABDD84A7066418F583561CB9906744E9AF405DA1\n"
+	"d /CTUE1YA.ADS.HB-JNB1424AB686D9308CA2EBA1D0D24A2C06C1B48CA004A248050667908CA004BF6\n"
 	"d /MSTEC7X.AT1.VT-ANE21409DCC3DD03BB52350490502B2E5129D5A15692BA009A08892E7CC831E210A4C06EEBC28B1662BC02360165C80E1F7\n"
 	"u - #MD/AA ATLTWXA.CR1.N856DN203A3AA8E5C1A9323EDD\n\n"
-	"2. Run decpdlc and pipe the the file contents on standard input:\n\n"
-	"\t./decpdlc < cpdlc_messages.txt\n\n"
-	"Supported FANS-1/A message types: CR1, CC1, DR1, AT1\n");
+	"2. Run decode_arinc and pipe the the file contents on standard input:\n\n"
+	"\t./decode_arinc < messages.txt\n\n");
 }
 
 void parse(char *txt, la_msg_dir msg_dir) {
-	int msgid = 0;
-//	dump_asn1 = 0;
-	char *s = strstr(txt, ".AT1");
-	if(s != NULL) {
-		msgid = 1;
-		goto msgid_set;
-	}
-	s = strstr(txt, ".CR1");
-	if(s != NULL) {
-		msgid = 1;
-		goto msgid_set;
-	}
-	s = strstr(txt, ".CC1");
-	if(s != NULL) {
-		msgid = 1;
-		goto msgid_set;
-	}
-	s = strstr(txt, ".DR1");
-	if(s != NULL) {
-		msgid = 1;
-		goto msgid_set;
-	}
-msgid_set:
-	if(msgid == 0) {
-		fprintf(stderr, "not a FANS-1/A CPDLC message\n");
-		return;
-	}
-	s += 4;
-	if(strlen(s) < 7) {
-		fprintf(stderr, "regnr not found\n");
-		goto end;
-	}
-	s += 7;
-	uint8_t *buf = NULL;
-	size_t buflen = la_slurp_hexstring(s, &buf);
-// cut off CRC
-	if(buflen >= 2)
-		buflen -= 2;
-// FIXME: prevent overflow on buflen cast
-	la_proto_node *node = la_cpdlc_parse(buf, (int)buflen, msg_dir);
-end:
+	la_proto_node *node = la_arinc_parse(txt, msg_dir);
 	printf("%s\n", txt);
 	if(node != NULL) {
 		la_vstring *vstr = la_proto_tree_format_text(NULL, node);
@@ -102,11 +60,14 @@ end:
 		la_vstring_destroy(vstr, true);
 	}
 	la_proto_tree_destroy(node);
-	free(buf);
 }
 
 int main(int argc, char **argv) {
 	la_msg_dir msg_dir = LA_MSG_DIR_UNKNOWN;
+	char *dump_asn1 = getenv("ENABLE_ASN1_DUMPS");
+	if(dump_asn1 != NULL && !strcmp(dump_asn1, "1")) {
+		la_config.dump_asn1 = true;
+	}
 	if(argc > 1 && !strcmp(argv[1], "-h")) {
 		usage();
 		exit(0);
