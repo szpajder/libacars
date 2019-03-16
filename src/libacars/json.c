367be799d983d286c17c0cca998c79381856d6e8
Tomasz Lemiech <szpajder@gmail.com>
2019-03-16 19:49:26 +0100

    JSON: allow null key values for scalar types
    Required for arrays.

--- src/libacars/json.c
+++ src/libacars/json.c
@@ -16,40 +16,44 @@ static void la_json_trim_comma(la_vstring * const vstr) {
 	}
 }
 
+static inline void la_json_print_key(la_vstring * const vstr, char const * const key) {
+	la_assert(vstr != NULL);
+	if(key != NULL && key[0] != '\0') {
+		la_vstring_append_sprintf(vstr, "\"%s\":", key);
+	}
+}
+
 void la_json_append_bool(la_vstring * const vstr, char const * const key, bool const val) {
 	la_assert(vstr != NULL);
-	la_assert(key != NULL);
-	la_vstring_append_sprintf(vstr, "\"%s\":%s,", key, (val == true ? "true" : "false"));
+	la_json_print_key(vstr, key);
+	la_vstring_append_sprintf(vstr, "%s,", (val == true ? "true" : "false"));
 }
 
 void la_json_append_double(la_vstring * const vstr, char const * const key, double const val) {
 	la_assert(vstr != NULL);
-	la_assert(key != NULL);
-	la_vstring_append_sprintf(vstr, "\"%s\":%f,", key, val);
+	la_json_print_key(vstr, key);
+	la_vstring_append_sprintf(vstr, "%f,", val);
 }
 
 void la_json_append_long(la_vstring * const vstr, char const * const key, long const val) {
 	la_assert(vstr != NULL);
-	la_assert(key != NULL);
-	la_vstring_append_sprintf(vstr, "\"%s\":%ld,", key, val);
+	la_json_print_key(vstr, key);
+	la_vstring_append_sprintf(vstr, "%ld,", val);
 }
 
 void la_json_append_string(la_vstring * const vstr, char const * const key, char const * const val) {
 	la_assert(vstr != NULL);
-	la_assert(key != NULL);
 	if(val == NULL) {
 		return;
 	}
-	la_vstring_append_sprintf(vstr, "\"%s\":\"%s\",", key, val);
+	la_json_print_key(vstr, key);
+	la_vstring_append_sprintf(vstr, "\"%s\",", val);
 }
 
 void la_json_object_start(la_vstring * const vstr, char const * const key) {
 	la_assert(vstr != NULL);
-	if(key == NULL || key[0] == '\0') {
-		la_vstring_append_sprintf(vstr, "%s", "{");
-	} else {
-		la_vstring_append_sprintf(vstr, "\"%s\":{", key);
-	}
+	la_json_print_key(vstr, key);
+	la_vstring_append_sprintf(vstr, "%s", "{");
 }
 
 void la_json_object_end(la_vstring * const vstr) {
@@ -60,8 +64,8 @@ void la_json_object_end(la_vstring * const vstr) {
 
 void la_json_array_start(la_vstring * const vstr, char const * const key) {
 	la_assert(vstr != NULL);
-	la_assert(key != NULL);
-	la_vstring_append_sprintf(vstr, "\"%s\":[", key);
+	la_json_print_key(vstr, key);
+	la_vstring_append_sprintf(vstr, "%s", "[");
 }
 
 void la_json_array_end(la_vstring * const vstr) {
