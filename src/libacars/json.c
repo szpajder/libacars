09c9f84a7db9fa00a3a0903ead46d41d40a7f085
Tomasz Lemiech <szpajder@gmail.com>
2019-03-17 12:50:08 +0100

    JSON: added character escaping
    Escaping is performed on string and char values. It's not performed
    on key names, as all key named in libacars are currently static.

--- src/libacars/json.c
+++ src/libacars/json.c
@@ -4,8 +4,10 @@
  *  Copyright (c) 2018-2019 Tomasz Lemiech <szpajder@gmail.com>
  */
 
+#include <string.h>			// strlen()
 #include <libacars/macros.h>		// la_assert()
 #include <libacars/vstring.h>		// la_vstring
+#include <libacars/util.h>		// LA_XCALLOC(), LA_XFREE()
 
 static void la_json_trim_comma(la_vstring * const vstr) {
 	la_assert(vstr != NULL);
@@ -16,9 +18,65 @@ static void la_json_trim_comma(la_vstring * const vstr) {
 	}
 }
 
+static char *la_json_escapechars(char const * const str) {
+	la_assert(str != NULL);
+	bool needs_escaping = false;
+	size_t orig_len = strlen(str);
+	size_t new_len = orig_len;
+	if(orig_len > 0) {
+		for(size_t i = 0; i < orig_len; i++) {
+			if(str[i] < ' ' || str[i] == '\"' || str[i] == '\\') {
+				needs_escaping = true;
+				new_len += 5;		// to fit the \uNNNN form
+			}
+		}
+	}
+	if(!needs_escaping) {
+		return strdup(str);
+	}
+	char *ret = LA_XCALLOC(new_len + 1, sizeof(char));
+	char *outptr = ret;
+	for(size_t i = 0; i < orig_len; i++) {
+		if(str[i] < ' ' || str[i] == '\"' || str[i] == '\\') {
+			*outptr++ = '\\';
+			switch(str[i]) {
+				case '\b':
+					*outptr++ = 'b';
+					break;
+				case '\t':
+					*outptr++ = 't';
+					break;
+				case '\n':
+					*outptr++ = 'n';
+					break;
+				case '\f':
+					*outptr++ = 'f';
+					break;
+				case '\r':
+					*outptr++ = 'r';
+					break;
+				case '\"':
+					*outptr++ = '\"';
+					break;
+				case '\\':
+					*outptr++ = '\\';
+					break;
+				default:
+					sprintf(outptr, "u%04x", str[i]);
+					outptr += 5;
+			}
+		} else {
+			*outptr++ = str[i];
+		}
+	}
+	return ret;
+}
+
 static inline void la_json_print_key(la_vstring * const vstr, char const * const key) {
 	la_assert(vstr != NULL);
 	if(key != NULL && key[0] != '\0') {
+// Warning: no character escaping is performed here. For libacars this is fine
+// as all key names are static. Escaping them would add unnecessary overhead.
 		la_vstring_append_sprintf(vstr, "\"%s\":", key);
 	}
 }
@@ -41,19 +99,21 @@ void la_json_append_long(la_vstring * const vstr, char const * const key, long c
 	la_vstring_append_sprintf(vstr, "%ld,", val);
 }
 
-void la_json_append_char(la_vstring * const vstr, char const * const key, char const  val) {
-	la_assert(vstr != NULL);
-	la_json_print_key(vstr, key);
-	la_vstring_append_sprintf(vstr, "\"%c\",", val);
-}
-
 void la_json_append_string(la_vstring * const vstr, char const * const key, char const * const val) {
 	la_assert(vstr != NULL);
 	if(val == NULL) {
 		return;
 	}
 	la_json_print_key(vstr, key);
-	la_vstring_append_sprintf(vstr, "\"%s\",", val);
+	char *escaped = la_json_escapechars(val);
+	la_vstring_append_sprintf(vstr, "\"%s\",", escaped);
+	LA_XFREE(escaped);
+}
+
+void la_json_append_char(la_vstring * const vstr, char const * const key, char const val) {
+	la_assert(vstr != NULL);
+	char tmp[2] = { val, '\0' };
+	la_json_append_string(vstr, key, tmp);
 }
 
 void la_json_object_start(la_vstring * const vstr, char const * const key) {
