fbd1cf82e8ba5f485df16e07241f1e5f5d33a097
Tomasz Lemiech <szpajder@gmail.com>
2019-12-14 18:00:03 +0100

    Added simple hash implementation.
    Needed for storing packet fragments for possible reassembly.

---
+++ libacars/hash.h
@@ -0,0 +1,34 @@
+/*
+ *  This file is a part of libacars
+ *
+ *  Copyright (c) 2018-2019 Tomasz Lemiech <szpajder@gmail.com>
+ */
+#ifndef LA_HASH_H
+#define LA_HASH_H 1
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct la_hash_s la_hash;
+typedef uint32_t (la_hash_func)(void const *key);
+typedef bool (la_hash_compare_func)(void const *key1, void const *key2);
+typedef void (la_hash_key_destroy_func)(void *key);
+typedef void (la_hash_value_destroy_func)(void *value);
+
+la_hash *la_hash_new(la_hash_func *compute_hash, la_hash_compare_func *compare_keys,
+	la_hash_key_destroy_func *destroy_key, la_hash_value_destroy_func *destroy_value);
+bool la_hash_insert(la_hash *h, void *key, void *value);
+bool la_hash_remove(la_hash *h, void *key);
+void *la_hash_lookup(la_hash const *h, void const *key);
+uint32_t la_hash_key_str(void const *k);
+bool la_hash_compare_str(void const *key1, void const *key2);
+void la_hash_destroy(la_hash *h);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !LA_HASH_H
