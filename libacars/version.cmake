a414a00db27e3d8088030c2f1d6801307b71ba05
Tomasz Lemiech <szpajder@gmail.com>
2020-01-23 22:52:11 +0100

    version.cmake: fixes backported from dumpvdl2
    - handle git release tags correctly (avoid double version number)
    - fix version.c output directory
    - cleanups

--- libacars/version.cmake
+++ libacars/version.cmake
@@ -1,21 +1,26 @@
+set (VERSION "char const * const LA_VERSION=\"")
 execute_process(COMMAND git describe --always --tags --dirty
-		OUTPUT_VARIABLE GIT_VERSION_
+		OUTPUT_VARIABLE GIT_VERSION
 		ERROR_QUIET
 		OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if ("${GIT_VERSION_}" STREQUAL "")
-	set(VERSION "char const * const LA_VERSION=\"${LA_VERSION}\";\n")
+if ("${GIT_VERSION}" STREQUAL "")
+	string(APPEND VERSION "${LA_VERSION}")
+elseif("${GIT_VERSION}" MATCHES ".+-g(.+)")
+	string(APPEND VERSION "${LA_VERSION}-${CMAKE_MATCH_1}")
+elseif("${GIT_VERSION}" MATCHES "v(.+)")
+	string(APPEND VERSION "${CMAKE_MATCH_1}")
 else()
-	string(REGEX REPLACE ".*-g" "" GIT_VERSION ${GIT_VERSION_})
-	set(VERSION "char const * const LA_VERSION=\"${LA_VERSION}-${GIT_VERSION}\";\n")
+	string(APPEND VERSION "${LA_VERSION}-${GIT_VERSION}")
 endif()
+string(APPEND VERSION "\";\n")
 
-if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/version.c)
-	file(READ ${CMAKE_CURRENT_SOURCE_DIR}/version.c VERSION_)
+if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/version.c)
+	file(READ ${CMAKE_CURRENT_BINARY_DIR}/version.c VERSION_)
 else()
 	set(VERSION_ "")
 endif()
 
 if (NOT "${VERSION}" STREQUAL "${VERSION_}")
-	file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/version.c "${VERSION}")
+	file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/version.c "${VERSION}")
 endif()
