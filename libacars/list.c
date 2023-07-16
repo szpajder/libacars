b5b86c6e2f310a289209ddbb8af620ec7e89d3b6
Tomasz Lemiech <szpajder@gmail.com>
2023-07-16 21:33:44 +0200

    Add routines for inserting and prepending elements to lists

--- libacars/list.c
+++ libacars/list.c
@@ -29,6 +29,50 @@ la_list *la_list_append(la_list *l, void *data) {
 	}
 }
 
+// Inserts a new element at position l->next.
+// Returns a pointer to the newly inserted element
+// (which might be a new list if l == NULL).
+la_list *la_list_insert(la_list *l, void *data) {
+	if(l == NULL) {
+		return la_list_append(l, data);
+	}
+	la_list *next = l->next;
+	l->next = NULL;
+	l = la_list_append(l, data);
+	l->next->next = next;
+	return l->next;
+}
+
+// Prepends a new element to the list l.
+// Returns a pointer to the new list head.
+la_list *la_list_prepend(la_list *l, void *data) {
+	LA_NEW(la_list, new);
+	new->data = data;
+	new->next = l;
+	return new;
+}
+
+// Inserts a new element into the list, preserving sort order of the elements.
+// The order is determined by the compare_nodes callback, which shall return a
+// value <0, 0 or >0, when the first argument is less, equal or greater than
+// the second one, respectively.  If list is NULL, a new list is allocated and
+// the element is appended to it.
+la_list *la_list_insert_sorted(la_list *list, void *data, la_list_compare_func *compare_nodes) {
+	if(list == NULL) {
+		return la_list_append(list, data);
+	}
+	if(compare_nodes(list->data, data) > 0) {
+		return la_list_prepend(list, data);
+	}
+	for(la_list *l = list; l->next != NULL; l = la_list_next(l)) {
+		if(compare_nodes(l->next->data, data) > 0) {
+			(void)la_list_insert(l, data);
+			return list;
+		}
+	}
+	return la_list_append(list, data);
+}
+
 size_t la_list_length(la_list const *l) {
 	size_t len = 0;
 	for(; l != NULL; l = la_list_next(l), len++)
--- libacars/list.h
+++ libacars/list.h
@@ -19,8 +19,13 @@ struct la_list {
 	la_list *next;
 };
 
+typedef int (la_list_compare_func)(void const *data1, void const *data2);
+
 la_list *la_list_next(la_list const *l);
 la_list *la_list_append(la_list *l, void *data);
+la_list *la_list_prepend(la_list *l, void *data);
+la_list *la_list_insert(la_list *l, void *data);
+la_list *la_list_insert_sorted(la_list *list, void *data, la_list_compare_func *compare_nodes);
 size_t la_list_length(la_list const *l);
 void la_list_foreach(la_list *l, void (*cb)(), void *ctx);
 void la_list_free(la_list *l);
